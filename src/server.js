488c58e566bad941f4812ba0268291b53c5e9004
Jessi <jessivelazq1@gmail.com>
2021-04-02 12:48:51 -0700

    this was a disaster

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
 const Data = require('./data.js');
 
+app.use(cors());
+const PORT = process.env.PORT || 3002;
+
+// const PORT = 'http://localhost:3001';
+
 app.use(express.urlencoded({extended:true}));
+
+app.get('/', function (request, response) {
+  response.send('Hello World');
+});
 
 app.get('/items', Data.getAllItems);
 app.get('/items/:id', getOneItem);
@@ -20,9 +30,11 @@ app.use( (error,req,res,next) => {
   res.status(500).send(`My Bad ... ${error.message}`);
 });
 
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
 module.exports = {
   server: app,
-  start: (port) => {
-    app.listen(port, console.log(`Server is up and running on port: ${port}`));
+  start: (PORT) => {
+    app.listen(PORT, console.log(`Server is up and running on port: ${PORT}`));
   },
 };
